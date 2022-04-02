ba0454461e98e0dcb5d3faf13d960d5562ca3237
Jayson Helseth <jayson.helseth@gmail.com>
2022-04-02 11:34:45 -0500

    Added code to make sure that we can deploy to the database.
    The test_db method will be replaced by the Xbee callback once we get to that piece of code.

--- data_receive_upload/main.py
+++ data_receive_upload/main.py
@@ -1,14 +1,42 @@
 import argparse
 import sys
+import asyncio
+import mysql.connector
+import uuid
+import time
+import json
 
 
 class ActionRunner:
     def __init__(self, dev, db):
         self.dev = dev
-        self.dbStr = db
+        db_str = json.loads(db)
+        self.connection = mysql.connector.connect(**db_str)
 
     def run(self):
         print("Doing stuff")
+
+        loop = asyncio.get_event_loop()
+        try:
+            self.test_db()
+            loop.run_forever()
+        finally:
+            loop.close()
+            # close db and device here
+            self.connection.close()
+
+    def test_db(self):
+        add_record = ("insert into Sensors values (%s, %s, CURRENT_TIMESTAMP())")
+        cursor = self.connection.cursor()
+
+        while True:
+            data = (str(uuid.uuid4()), f'{{ "data": "{uuid.uuid4()}"}}')
+            print(data)
+            cursor.execute(add_record, data)
+            self.connection.commit()
+            time.sleep(1)
+
+        cursor.close()
 
 
 def main():
